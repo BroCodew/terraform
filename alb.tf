a52d22839f84a3ce415918c553cfe1d080d9b1a2
AdamSmith86 <adam.smith@tqcsolution.com>
2026-05-15 01:49:59 +0700

    add ecs service ecs

--- alb.tf
+++ alb.tf
@@ -48,46 +48,46 @@ resource "aws_security_group" "ecs_task" {
   }
 }
 
-resource "aws_lb" "app"{
-    name = "app-alb-tr"
-    internal = false
-    load_balancer_type = "application"
-    security_groups = [aws_security_group.alb.id]
-    subnets = [aws_subnet.public.id, aws_subnet.public_b.id]
-    tags = {
-        Name = "app-alb-tr" 
-    }
+resource "aws_lb" "app" {
+  name               = "app-alb-tr"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = [aws_subnet.public.id, aws_subnet.public_b.id]
+  tags = {
+    Name = "app-alb-tr"
+  }
 
 }
 
 resource "aws_lb_target_group" "app" {
-    name = "app-tg-tr"
-    port = 3000
-    protocol = "HTTP"
-    target_type = "ip"
-    vpc_id = aws_vpc.main.id
-    health_check {
-        path = "/"
-        interval = 30
-        timeout = 5
-        healthy_threshold = 2
-        unhealthy_threshold = 2
-        matcher = "200-399"    
-    }
-    tags = {
-        Name = "app-tg-tr" 
-    }
+  name        = "app-tg-tr"
+  port        = 3000
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-399"
+  }
+  tags = {
+    Name = "app-tg-tr"
+  }
 }
 
 resource "aws_lb_listener" "app" {
-    load_balancer_arn = aws_lb.app.arn
-    port = 80
-    protocol = "HTTP"
-    default_action {
-        type = "forward"
-        target_group_arn = aws_lb_target_group.app.arn
-    }
-    tags = {
-        Name = "app-listener-tr" 
-    }
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+  tags = {
+    Name = "app-listener-tr"
+  }
 }
--- ecs.tf
+++ ecs.tf
@@ -43,3 +43,27 @@ resource "aws_ecs_task_definition" "app" {
     Name = "my-app-task-tr"
   }
 }
+
+
+resource "aws_ecs_service" "app" {
+  name            = "my-app-service-tr"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 2
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+    security_groups  = [aws_security_group.ecs_task.id]
+    assign_public_ip = false
+
+  }
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.app.arn
+    container_name   = "my-app"
+    container_port   = 3000
+  }
+
+  depends_on = [aws_lb_listener.app]
+
+}
